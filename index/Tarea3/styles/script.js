2e48d75aceaefa2aec64d99d771a838d62bcee69
SantyMCS <santymartin014@gmail.com>
2025-11-03 23:21:10 -0600

    Update cambiarKirby function for new states

--- index/Tarea3/styles/script.js
+++ index/Tarea3/styles/script.js
@@ -1,20 +1,21 @@
 function cambiarKirby(estado) {
   const kirby = document.getElementById("personaje");
 
-  if (estado === "fire") {
-    kirby.src = "img/kirby_fire.png";
-    document.body.style.backgroundImage = "url('img/bg_fire.jpg')";
+  if (estado === "volcano") {
+    kirby.src = "https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/kirby_volcano.png";
+    document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/bg_volcano.jpg')";
   } 
   else if (estado === "ice") {
-    kirby.src = "img/kirby_ice.png";
-    document.body.style.backgroundImage = "url('img/bg_ice.jpg')";
+    kirby.src = "https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/kirby_ice.png";
+    document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/bg_ice.jpg')";
   } 
   else if (estado === "sword") {
-    kirby.src = "img/kirby_sword.png";
-    document.body.style.backgroundImage = "url('img/bg_sword.jpg')";
+    kirby.src = "https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/kirby_sword.png";
+    document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/bg_normal.jpg')";
   } 
   else {
-    kirby.src = "img/kirby_normal.png";
-    document.body.style.backgroundImage = "url('img/bg_normal.jpg')";
+    kirby.src = "https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/kirby_normal.png";
+    document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Santy014/santy.github.io/refs/heads/main/index/Tarea3/Imagenes/bg_normal.jpg')";
   }
 }
+
